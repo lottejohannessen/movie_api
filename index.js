fd8b387259d086b10894a05a8bb19b2248691c76
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-05-29 10:54:04 -0700

    fix cors issue

--- index.js
+++ index.js
@@ -14,18 +14,8 @@ require('./passport');
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080','http://testsite.com'];
+app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesnt allow access from origin' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 // app.use(morgan('common'));
 
